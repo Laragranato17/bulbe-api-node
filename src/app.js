68ad7975fba5dd89b15dc6b5111251b13095dbd7
Lara Granato <laragrnato17@gmail.com>
2026-03-12 14:46:15 -0300

    fix: mover middleware express.json() antes das rotas para corrigir req.body undefined

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const produtosRouter = require('./routes/produtos');
 
 app.use('/api/v1/produtos', produtosRouter);
 
 
-app.use(express.json());
+
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${new Date().toISOString()}`);
